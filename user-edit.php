b426f145986c96054df66c2cd2e4e0a98d1c31ae
maaz1708 <maazshaikh9141@gmail.com>
2024-10-01 17:41:56 -0400

    Add files via upload

--- user-edit.php
+++ user-edit.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Updates network administration panel.
+ * Edit user administration panel.
  *
  * @package WordPress
- * @subpackage Multisite
+ * @subpackage Administration
  * @since 3.1.0
  */
 
 /** Load WordPress Administration Bootstrap */
 require_once __DIR__ . '/admin.php';
 
-require ABSPATH . 'wp-admin/update-core.php';
+require ABSPATH . 'wp-admin/user-edit.php';
